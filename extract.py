205b9bba6529e16c04aafe55ea79317f1b54d660
phan chi khang <pckhang10@gmail.com>
2021-07-09 01:32:23 +0700

    New version update

--- extract.py
+++ extract.py
@@ -74,155 +74,287 @@ class InvoiceProcessor(object):
         self._text = text
         self._dataframe = {}
         self._result = []
+        self._file_pass = True
+    
 
     def process_text(self):
         # remove empty lines
         self._text = "\n".join([line for line in self._text.split("\n") if line.strip() != ''])
-        # split by "Bestselling:"
-        items = self._text.split("Bestelling:")
-        # list of result
-        self._result = []
-        track_number = 1
-        flag_stop = False
-        for item in items[1:]:
-            # remove orphan item
-            if "Discount Remake" in item:
-                continue
-            item = item.strip()
-            # bring back "Bestelling:" to item
-            item = "Bestelling:" + item
-            #print(item[1])
-            line_number = 1
-            each_items = []
-            for line_item in item.split("\n"):
-                if flag_stop == True:
-                    continue
-                if "Levering" in line_item:
-                    flag_stop = True
-                    continue
-                # line 1 for Bestelling, Datum bestelling, ref
-                # line 2 for Totallbedrag Exc BTW
-                # remanin lines for detil
-                # merge mutiple spaces to one space
-                line_item = re.sub(r"\s+", " ", line_item).strip()
-                #print(line_item)
-                each_item = []
-                if line_number == 1:
-                    each_items = each_items + self.handle_for_first_line(line_item)
-                elif line_number == 2:
-                    each_items.append(self.handle_for_second_line(line_item))
-                else:
-                    each_item_child = self.handle_for_remain_lines(line_item)
-                    each_item.append(each_item_child)
-                    each_items.append(each_item)
-                line_number = line_number + 1
-            self._result.append(each_items)
-            track_number += 1
-        #print(result)
-        return self._result
+        # for each line
+        result = {}
+        index = 0
+        temp_result = []
+        for line in self._text.split("\n"):
+            # merge multiple space to one space
+            pattern = re.compile(r'\s+')
+            line = re.sub(pattern, ' ', line)
+            # handle for each row
+            if "Datum bestelling:" in line and "ref:" in line:
+                index = index + 1
+                # split to get each item for bestelling, datum bestelling, ref_val
+                bestelling, datatum_bestelling, ref = self.handle_for_bestelling(line)
+                result[index] = []
+                result[index].append(bestelling)
+                result[index].append(datatum_bestelling)
+                result[index].append(ref)
+                # if temp_result is not None:
+                #     result[index].append(temp_result)
+                #     # reset temp_result
+                #     temp_result = []
+            elif "Totaalbedrag Exc BTW" in line:
+                # handle for totaalbedragexcbtw
+                totaal = self.handle_for_totaal(line)
+                if (totaal is None and self._file_pass == False) or (totaal is not None and self._file_pass == True):
+                    if totaal is None:
+                        totaal = "N/A"
+                    result[index].append(totaal)
+                    if temp_result is not None:
+                        result[index].append(temp_result)
+                        # reset temp_result
+                        temp_result = []
+            else:
+                if 'x' in line and '%' in line and EURO_SIGN in line:
+                    line_item, dimension_1, dimension_2, bwt, prijs, totaal = self.handle_for_line_item_dimesions_so_on(line)
+                    temp_result.append([line_item, dimension_1, dimension_2, bwt, prijs, totaal])
+
+        return result
+
+
+    def extract(self):
+        result = self.process_text()
+        f_data = []
+        for _, value in result.items():
+            data = []
+            for ele in value[:-1]:
+                data.append(ele)
+            for ele in value[-1]:
+                f_data.append(data + ele)
+        return f_data
+                
+    def handle_for_bestelling(self, text):
+        """
+        example line: 
+        Good: Bestelling: 264306, Datum bestelling: 2021-05-10, ref: B 100068766 Lucian Simo
+        Bad: Datum bestelling: 2021-06-11 (Bestelling: 270863, ref: PCB 100070747 Pascal Janssen)    Datum of Aflever: 2021-06-28 (Levering GLS tracking #:646830032560)
+
+        Return (264306, 2021-05-10, B 100068766)
+        """
+        if "(Bestelling:" in text:
+            self._file_pass = False
+            text = text.split(')')[0].replace('(', '')
+            bestelling = text[(text.find("Bestelling:") + len("Bestelling:")):text.find("ref:")]
+            datum_bestelling = text[(text.find("Datum bestelling:") + len("Datum bestelling:")):text.find("Bestelling:")]
+        else:
+            bestelling = text[(text.find("Bestelling:") + len("Bestelling:")):text.find("Datum bestelling:")]
+            datum_bestelling = text[(text.find("Datum bestelling:") + len("Datum bestelling:")):text.find("ref:")]
+        ref = text.split("ref:")[1]
+
+        return (self.remove_special_characters(bestelling), self.remove_special_characters(datum_bestelling), self.remove_special_characters(ref))
+    
+    def handle_for_totaal(self, text):
+        try:
+            return self.remove_special_characters(text.split("Totaalbedrag Exc BTW:")[1])
+        except IndexError as e:
+            return None
+    
+    def handle_for_line_item_dimesions_so_on(self, text):
+        """
+        Example line
+        1 x houten jaloezieën 50mm 0.00% € 93.04  € 93.04[1160 x 1770 mm]
+        """
+        # split "%""
+        items = text.split("%")
+        line_item = " ".join(items[0].split(" ")[:-1])
+        bwt = " ".join([items[0].split(" ")[-1],"%"])
+        child_item = [ele.strip() for ele in items[1].strip().split(EURO_SIGN) if ele != '']
+        prijs = child_item[0]
+        if '[' in text and ']' in text:
+            dimension_1 = child_item[1].split('[')[1].split("x")[0].strip()
+            dimension_2 = child_item[1].split('[')[1].split("x")[1].strip().replace(']', '')
+            totaal = child_item[1].split('[')[0]
+        else:
+            dimension_1 = 0
+            dimension_2 = 0
+            totaal = child_item[1]
+
+        return (line_item, dimension_1, dimension_2, bwt, prijs, totaal)
+
+    
+    @staticmethod
+    def remove_special_characters(text):
+        pattern = r'[:|,|€]'
+        return re.sub(pattern, '', text).strip()
 
     def build_dataframe(self):
+
+        result = self.extract()
         global row
         # handle result
-        bestelling = []
-        datum = []
-        ref = []
-        totaal_btc = []
-        count = []
-        line_item = []
-        dimension1 = []
-        dimension2 = []
-        btw = []
-        prij = []
-        totaal = []
-        for item in self._result:
-            for child_item in item[-1]:
-                bestelling.append(item[0])
-                datum.append(item[1])
-                ref.append(item[2])
-                totaal_btc.append(item[3])
-                group_values = child_item[0].split(" x ")
-                if "[" in child_item[0]:
-                    count.append(group_values[0].strip())
-                    line_item.append(group_values[1].split("[")[0].strip())
-                    dimension1.append(group_values[1].split("[")[1].strip())
-                    dimension2.append(group_values[2].replace("mm]", "").strip())
-                else:
-                    line_item.append(group_values[1].strip())
-                    dimension1.append(0)
-                    dimension2.append(0)
-                btw.append(child_item[1])
-                prij.append(child_item[2])
-                totaal.append(child_item[3])
-        self._dataframe['Bestelling'] = bestelling
-        self._dataframe['Datum bestelling'] = datum
-        self._dataframe['ref'] = ref
-        self._dataframe['totaalbedrag exc BTW'] = totaal_btc
-        self._dataframe['line item'] = line_item
-        self._dataframe['dimension 1 (mm)'] = dimension1
-        self._dataframe['dimension 2 (mm)'] = dimension2
-        self._dataframe['btw'] = btw
-        self._dataframe['prijs (ex)'] = prij
-        self._dataframe['totaal (ex)'] = totaal
+        self._dataframe['Bestelling'] = []
+        self._dataframe['Datum bestelling'] = []
+        self._dataframe['ref'] = []
+        self._dataframe['totaalbedrag exc BTW'] = []
+        self._dataframe['line item'] = []
+        self._dataframe['dimension 1 (mm)'] = []
+        self._dataframe['dimension 2 (mm)'] = []
+        self._dataframe['btw'] = []
+        self._dataframe['prijs (ex)'] = []
+        self._dataframe['totaal (ex)'] = []
+        for ele in result:
+            self._dataframe['Bestelling'].append(ele[0])
+            self._dataframe['Datum bestelling'].append(ele[1])
+            self._dataframe['ref'].append(ele[2])
+            self._dataframe['totaalbedrag exc BTW'].append(ele[3])
+            self._dataframe['line item'].append(ele[4])
+            self._dataframe['dimension 1 (mm)'].append(ele[5])
+            self._dataframe['dimension 2 (mm)'].append(ele[6])
+            self._dataframe['btw'].append(ele[7])
+            self._dataframe['prijs (ex)'].append(ele[8])
+            self._dataframe['totaal (ex)'].append(ele[9])
 
         return self._dataframe
 
 
-    def handle_for_first_line(self, line_text):
-        """
-        Handle first line with Bestelling, Datum bestelling, ref format
-        args: receive a line text
-        return values of [Bestelling, Datum bestelling, ref]
-        """
-        bestelling_value = self.split_to_get_value(line_text, BESTELLING, DATUM_BESTELLING)
-        datum_value = self.split_to_get_value(line_text, DATUM_BESTELLING, REF)
-        ref_value = line_text.split(REF)[-1].strip()
 
-        return [bestelling_value, datum_value, ref_value]
+class AnwisProcessor(object):
+    CATALOGUE = "Catalogue no. Order No"
+    ITEM_NAME = "Item Name"
+    DIMESION_1 = "Dimension 1"
+    DIMESION_2 = "Dimension 2"
+    ITEM_NO = "Item No"
+    REF = "ref"
+    CUSTOMER = "Customer"
+    QUANTITY = "Quantity"
+    NET_PRICE = "Net Price"
+    NET_PRICE_AFTER_DISCOUNT = "Net Price After Discount"
+    NET_AMOUNT = "Net Amount After Discount"
+    GROSS_AMOUNT = "Grossamount After Discount"
 
-    def handle_for_second_line(self, line_text):
-        """
-        Totallbedrag Exc BTW
-        args line text contain Totallbedrag Exc BTW
-        return value of Totallbedrag Exc BTW
-        """
-        total_value = line_text.split(TOTAL)[1].replace(":", "").strip()
-        return total_value
-    
-    def handle_for_remain_lines(self, line_item):
-        """
-        Hanle for remain lines
-        args line text
-        return list of item child
-        """
-        each_item_child = []
-        if "[" in line_item:
-            # find index of charactar "["
-            line_item = re.split(r'%', line_item)
-            #line_item_value = " ".join(line_item[0].split(" ")[:-1])
-            line_item_value = "{line_item_0} [{value_mm}".format(
-                line_item_0=" ".join(line_item[0].split(" ")[:-1]),
-                value_mm=line_item[1].split("[")[-1]
-            )
-            #line_item = re.split(r'\d{1}mm', line_item)
-            #line_item_value = "{line_item_0}mm [{value_mm}".format(line_item_0=line_item[0],
-            #                                                    value_mm=line_item[1].split("[")[-1])
-            group_values = line_item[1].split("[")[0].strip().replace(EURO_SIGN, "").split(" ")
-            each_item_child = [value for value in group_values if value != '']
-            each_item_child.insert(0, line_item[0].split(" ")[-1].strip() + "%")
-            each_item_child.insert(0, line_item_value)
+    def __init__(self):
+        pass
+
+    def read_pdf_file(self, pdf_file):
+        tables = read_pdf(pdf_file, pages='all', encoding='utf-8')
+        return tables
+
+    def main(self, pdf_file):
+        result = {}
+        tables = self.read_pdf_file(pdf_file)
+        for table in tables:
+            # convert table as dataframe to json
+            data = self.convert_table_dataframe_to_json(table)
+            # extract for each table
+            data_result = self.extract_data(data)
+            #print(data_result)
+            if len(data_result[self.ITEM_NAME]) != 0:
+                if not result:
+                    result = data_result
+                else:
+                    for key in result.keys():
+                        result[key] = result[key] + data_result[key]
+        return result
+
+
+
+    def extract_data(self, data):
+        key_int = 0
+        result = {}
+        result[self.ITEM_NAME] = []
+        result[self.DIMESION_1] = []
+        result[self.DIMESION_2] = []
+        result[self.ITEM_NO] = []
+        result[self.REF] = []
+        result[self.CUSTOMER] = []
+        result[self.QUANTITY] = []
+        result[self.NET_PRICE] = []
+        result[self.NET_PRICE_AFTER_DISCOUNT] = []
+        result[self.NET_AMOUNT] = []
+        result[self.GROSS_AMOUNT] = []
+        #print(json.dumps(data, indent=4))
+        #sys.exit(0)
+        for key, value in data.items():
+            for key_c, value_c in value.items():
+                if key_c == "Net" and value_c is not None and value_c.replace(",", "").isdigit():
+                    #print(key, value)
+                    item_dimensions = data[str(key_int + 1)][self.CATALOGUE]
+                    item_no_ref_customer = data[str(key_int + 2)][self.CATALOGUE]
+                    # process_data
+                    item_name, dimension_1, dimension_2 = self.process_item_dimensions(item_dimensions)
+                    item_no, ref, customer = self.process_item_no_ref_customer(item_no_ref_customer)
+                    try:
+                        quantity = value["Unnamed: 2"]
+                        net_price = value["Unnamed: 4"]
+                        net_price_afer_discount = value["Net"]
+                        net_mount = value["Net.1"]
+                        if 'Gross' in value.keys():
+                            gross_amount = value["Gross"]
+                        else:
+                            gross_amount = value["Unnamed: 8"]
+                    except Exception as e:
+                        quantity = value["Quantity"]
+                        net_price = value["Net"]
+                        net_price_afer_discount = value["Net  price"]
+                        net_mount = value["Net amount"]
+                        gross_amount = value["Grossamount after"]
+
+                    result[self.ITEM_NAME].append(item_name)
+                    result[self.DIMESION_1].append(dimension_1)
+                    result[self.DIMESION_2].append(dimension_2)
+                    result[self.ITEM_NO].append(item_no)
+                    result[self.REF].append(ref)
+                    result[self.CUSTOMER].append(customer)
+                    result[self.QUANTITY].append(quantity)
+                    result[self.NET_PRICE].append(net_price)
+                    result[self.NET_PRICE_AFTER_DISCOUNT].append(net_price_afer_discount)
+                    result[self.NET_AMOUNT].append(net_mount)
+                    result[self.GROSS_AMOUNT].append(gross_amount)
+
+            key_int = key_int + 1
+
+        return result
+
+    @staticmethod
+    def process_item_dimensions(text):
+        if "/" not in text:
+            dimension_1 = 0
+            dimension_2 = 0
+            item_name = text
         else:
-            line_item = line_item.replace(EURO_SIGN, "")
-            line_item = re.sub(r'\s+', ' ', line_item).strip()
-            line_item = line_item.split(" ")
-            each_item_child = [" ".join([line_item[0], line_item[1], line_item[2]])] + [line_item[-3], line_item[-2], line_item[-1]]
+            values = text.split("/")
+            dimension_1 = values[0].split(" ")[-1].strip()
+            dimension_2 = values[1].split(" ")[0].strip()
+            item_name = " ".join(values[0].split(" ")[:-1])
         
-        return each_item_child
+        return (item_name, dimension_1, dimension_2)
 
-    def split_to_get_value(self, text, keyword_start, keyword_end):
-        value = text[text.find(keyword_start) + len(keyword_start):text.rfind(keyword_end)]
-        return value.replace(",", "").strip()
+    @staticmethod
+    def process_item_no_ref_customer(text):
+        values = text.split(" ")
+        item_no = values[0]
+        if len(values) == 1:
+            ref = "N/A"
+            customer = "N/A"
+        elif len(values) == 2:
+            ref = "N/A"
+            customer = values[1]
+        else:
+            if values[1].isdigit():
+                ref = values[1]
+                customer = " ".join(values[2:])
+            elif values[2].isdigit() and len([val for val in values if val.strip() != '']) > 3:
+                ref = " ".join([values[1], values[2]])
+                customer = " ".join(values[2:])
+            else:
+                ref = "N/A"
+                customer = " ".join(values[1:])
+        
+        return (item_no, ref, customer)
 
+    @staticmethod
+    def convert_table_dataframe_to_json(table):
+        parsed = json.loads(table.to_json(orient="index"))
+        #print(json.dumps(parsed, indent=4))
+        return parsed
 
 def process_text(text, type_extract="invoice"):
     if type_extract == "invoice":
@@ -299,12 +431,13 @@ def main(args):
     
     for pdf_file in glob.glob(pdf_folder + "/*.pdf"):
         try:
-            # read pdf file
-            text = convert_pdf_to_txt(pdf_file)
-            print(text)
-            sys.exit(0)
-            # process the data
-            dataframe = process_text(text)
+            if args.type == "invoices":
+                # read pdf file
+                text = convert_pdf_to_txt(pdf_file)
+                # process the data
+                dataframe = process_text(text)
+            elif args.type == "anwis":
+                dataframe = AnwisProcessor().main(pdf_file)
             save_to_excel_file(dataframe, output_file)
             print("{pdf_file} completed without any issues".format(pdf_file=pdf_file))
         except Exception:
